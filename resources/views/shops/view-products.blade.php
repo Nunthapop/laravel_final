cbcef04af4b1130f54fe8f514cb47a2d889da1c0
Arthur <nuntapopsangehot@gmail.com>
2024-09-13 22:45:58 +0700

    Shops view-products

--- resources/views/shops/view-products.blade.php
+++ resources/views/shops/view-products.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends('layouts.main')
+@extends('layouts.main')
 @section('title',$title)
 
 @section('content')
@@ -10,34 +10,43 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
-    </head>
-    <form action="{{ route('shops.list') }}" method="get" class="search-form">
+    {{-- </head> --}}
+    <form action="{{ route('shops.view-products' , ['shop' => $shops->code]) }}" method="get" class="search-form">
         <label>
             Search
             <input type="text" name="term" value="{{ $search['term'] }}" />
         </label>
+        <label>
+            Min Price
+            <input type="number" name="minPrice" value="{{ $search['minPrice'] }}" step="any" />
+        </label><br />
+        <label>
+            Max Price
+            <input type="number" name="maxPrice" value="{{ $search['maxPrice'] }}" step="any" />
+        </label><br />
         <button type="submit" class="primary">Search</button>
-        <a href="{{ route('shops.list') }}">
+        <a href="{{ route('shops.view-products' , ['shop' => $shops->code]) }}">
             <button type="button" class="accent">Clear</button>
         </a>
     </form>
-    <div>{{ $shop->withQueryString()->links() }}</div>
+    <div>{{ $products->withQueryString()->links() }}</div>
     
         <table class="lg:w-1/2">
             <tr>
                 <th>Code</th>
                 <th>Name</th>
                 <th>Owner</th>
-                <th>No. of products</th>
+             
             </tr>
             <tbody>
                 <tr>
-                    @foreach ($shop as $shops)
-                        <td class="underline"> <a href="{{route ('shops.view',['shop' => $shops->code,])}}"> {{ $shops->code }} </a> </td>
-                        <td> {{ $shops->name }}</td>
-                        <td> {{ $shops->owner }}</td>
-                        <td> {{ $shops->products_count }}</td>
-                        
+                    @foreach ($products as $product)
+                        <td class="underline font-bold hover:text-blue-600"> <a
+                                href="{{ route('products.view', ['product' => $product->code]) }}"> {{ $product->code }}
+                            </a> </td>
+                        <td> {{ $product->name }}</td>
+                        <td> {{ $product->price }}</td>
+                      
                 </tr>
                 @endforeach
             </tbody>
@@ -47,4 +56,4 @@
    
 
     </html>
-@endsection --}}
+@endsection
