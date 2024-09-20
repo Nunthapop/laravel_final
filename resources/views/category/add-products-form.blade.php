b83b1bbd224d27a252984486c9006807c28f11b1
Arthur <nuntapopsangehot@gmail.com>
2024-09-20 11:32:19 +0700

    .

--- resources/views/category/add-products-form.blade.php
+++ resources/views/category/add-products-form.blade.php
@@ -13,14 +13,15 @@
     </head>
     <nav>
         <ul>
-            <li><a href="{{ route('category.view-products', [
-                'cateCode' => $category->code,
-            ]) }}">&lt; Back</a></li>
+            <li><a
+                    href="{{ route('category.view-products', [
+                        'cateCode' => $category->code,
+                    ]) }}">&lt;
+                    Back</a></li>
         </ul>
     </nav>
     {{-- ['shop' => $shops->code] sent to addProduct  --}}
-    <form action="{{ route('category.add-product', ['cateCode' => $category->code]) }}" 
-        method="post" class="search-form">
+    <form action="{{ route('category.add-product', ['cateCode' => $category->code]) }}" method="post" class="search-form">
         @csrf
         <label>
             Search
@@ -30,27 +31,36 @@
         <a href="{{ route('category.add-products-form', ['cateCode' => $category->code]) }}">
             <button type="button" class="accent">Clear</button>
         </a>
-    <div>{{ $products->withQueryString()->links() }}</div>
-    <table class="lg:w-1/2">
-        <tr>
-            <th>Code</th>
-            <th>Name</th>
-            <th>Price</th>
-        </tr>
-        <tbody>
+        <label>
+            Min Price
+            <input type="number" name="minPrice" value="{{ $search['minPrice'] }}" step="any" />
+        </label><br />
+        <label>
+            Max Price
+            <input type="number" name="maxPrice" value="{{ $search['maxPrice'] }}" step="any" />
+        </label><br />
+        <div>{{ $products->withQueryString()->links() }}</div>
+        <table class="lg:w-1/2">
             <tr>
-                @foreach ($products as $product)
-                    <td class="underline">
-                        {{ $product->code }} </td>
-                    <td> {{ $product->name }}</td>
-                    <td> {{ $product->price }}</td>
-                
-                    <td> <button type="submit" name="category" value="{{ $product->code }}" class="primary">Add</button></td>
+                <th>Code</th>
+                <th>Name</th>
+                <th>Price</th>
             </tr>
-            @endforeach
-        </tbody>
-    </table>
+            <tbody>
+                <tr>
+                    @foreach ($products as $product)
+                        <td class="underline">
+                            {{ $product->code }} </td>
+                        <td> {{ $product->name }}</td>
+                        <td> {{ $product->price }}</td>
+
+                        <td> <button type="submit" name="category" value="{{ $product->code }}"
+                                class="primary">Add</button></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
 
     </html>
-</form>
+    </form>
 @endsection
